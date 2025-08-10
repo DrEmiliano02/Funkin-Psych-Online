46f0141452a2264fd290e0b4055f5a45bae67381
DrEmiliano02 <dremiliano02@gmail.com>
2025-08-09 18:04:48 -0600

    pwro

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -3,13 +3,15 @@ package objects;
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
+	private var isOldIcon:Bool = false;
 	public var isPlayer:Bool = false;
-	public var hasWinning:Bool = true;
-	public var char:String = '';
+	private var char:String = '';
+	public var ox:Int;
 
-	public function new(char:String = 'face', isPlayer:Bool = false, ?allowGPU:Bool = true)
+	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
 	{
 		super();
+		isOldIcon = (char == 'bf-old');
 		this.isPlayer = isPlayer;
 		changeIcon(char, allowGPU);
 		scrollFactor.set();
@@ -20,42 +22,26 @@ class HealthIcon extends FlxSprite
 		super.update(elapsed);
 
 		if (sprTracker != null)
-			if(!ClientPrefs.data.ogIconBop){
-				setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
-			}else{
-				setPosition(sprTracker.x + sprTracker.width + 10, sprTracker.y - 30);
-			}
+			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
 	}
 
 	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String, ?allowGPU:Bool = true) {
 		if(this.char != char) {
-			var curAnimation:Int = 0;
-			if (this.animation.curAnim != null) curAnimation = this.animation.curAnim.curFrame; // quick patch so icons doesn't return to their default animation after changing it
-			var hasWin:Bool = false;
-			var singleIcon:Bool = false;
-
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 			
 			var graphic = Paths.image(name, allowGPU);
-			if (graphic.width == 450) hasWin = true;
-			else if (graphic.width == 150) singleIcon = true;
 			var iSize:Float = Math.round(graphic.width / graphic.height);
-			var realSize:Float = (hasWin ? 3 : (singleIcon ? 1 : iSize));
-			loadGraphic(graphic, true, Math.floor(graphic.width / realSize), Math.floor(graphic.height));
-			iconOffsets[0] = (width - 150) / realSize;
+			loadGraphic(graphic, true, Math.floor(graphic.width / iSize), Math.floor(graphic.height));
+			iconOffsets[0] = (width - 150) / iSize;
 			iconOffsets[1] = (height - 150) / iSize;
 			updateHitbox();
 
-			var animArray:Array<Int> = [];
-			for(i in 0...3) animArray.push((i <= frames.frames.length - 1 ? i : 0)); // 0: Default | 1: Losing | 2: Winning
-			animation.add(char, animArray, 0, false, isPlayer);
+			animation.add(char, [for(i in 0...frames.frames.length) i], 0, false, isPlayer);
 			animation.play(char);
-			this.animation.curAnim.curFrame = curAnimation;
 			this.char = char;
-			this.hasWinning = hasWin;
 
 			if(char.endsWith('-pixel'))
 				antialiasing = false;
@@ -64,17 +50,11 @@ class HealthIcon extends FlxSprite
 		}
 	}
 
-	public var autoAdjustOffset:Bool = true;
 	override function updateHitbox()
 	{
 		super.updateHitbox();
-		if(autoAdjustOffset)
-		{
-			if(!ClientPrefs.data.ogIconBop){
-				offset.x = iconOffsets[0];
-				offset.y = iconOffsets[1];
-			}
-		}
+		offset.x = iconOffsets[0];
+		offset.y = iconOffsets[1];
 	}
 
 	public function getCharacter():String {
